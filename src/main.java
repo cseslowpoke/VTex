943d2ba2f5cefebe949777f543897458b738ec8a
Guan <guansdrailib@gmail.com>
2022-06-08 21:28:29 +0800

    GUI

--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        window2 w = new window2();
-        w.add(w.$$$getRootComponent$$$());
-        w.setSize(500, 500);
-        w.setVisible(true);
-    }
-}
